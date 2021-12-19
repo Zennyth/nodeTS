d0f72669f0f30940c1d8e238ebe7162b2ebe120d
Zennyth <matsfox@orange.fr>
2021-12-19 20:09:07 +0100

    update

--- api/src/db/init.db.ts
+++ api/src/db/init.db.ts
@@ -2,8 +2,6 @@ import { isDeepStrictEqual } from 'util';
 import {Sensor, Emergency, Station, Team} from './models/';
 const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test';
 
-console.log(isDev)
-
 const dbInit = () => {
   Emergency.sync({ force: isDev });
   Sensor.sync({ force: isDev });
