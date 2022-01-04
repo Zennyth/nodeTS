55a9a619d59e0345bb342b8caa87e9dedd9a98da
Arthur <arthur.saunier2@gmail.com>
2022-01-04 11:11:06 +0100

    clean mqtt utils

--- gateway/src/utils/mqtt.utils.ts
+++ gateway/src/utils/mqtt.utils.ts
@@ -1,5 +1,3 @@
-import { MqttClient } from "mqtt";
-
 const mqtt = require('mqtt');
 const clientId = "clientTest"
 const connectUrl = `mqtt://${process.env.BROKER_URL}`
@@ -11,21 +9,20 @@ const OPTIONS = {
     //username: 'emqx',
     //password: 'public',
     reconnectPeriod: 1000,
-  }
+}
 
 export const client = mqtt.connect(connectUrl, OPTIONS)
-
 
 export const subscribe = (topic: string) => {
     client.subscribe([topic], () => {
         console.log(`Subscribe to topic '${topic}'`)
-      })
-  }
+    })
+}
 
 export const publish = (topic: string) => {
-    client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: true }, (error) => {
         if (error) {
           console.error(error)
         }
-      })
+    })
 }
